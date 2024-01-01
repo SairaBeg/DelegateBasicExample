96bc185840cb0562c1745fdc1cc6632e3c594404
SairaBeg <sairabeg.dev@gmail.com>
2024-01-01 16:51:52 -0500

    Initial commit

---
+++ DelegateBasicExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DelegateBasicExample
+{
+    class Program
+    {
+        delegate void LogDel(string text);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            LogText("pass text");
+
+        }
+        static void LogText(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+    }
+}
