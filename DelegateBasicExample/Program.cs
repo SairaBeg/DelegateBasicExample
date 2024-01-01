179e96239fffdaef73c2075ed391f193bdc1c767
SairaBeg <sairabeg.dev@gmail.com>
2024-01-01 17:31:58 -0500

    Update Program.cs

--- DelegateBasicExample/Program.cs
+++ DelegateBasicExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DelegateBasicExample
 {
@@ -8,13 +9,47 @@ namespace DelegateBasicExample
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Start");
-            LogText("pass text");
+            Log log = new Log();
+
+            LogDel LogTextToScreenDel,LogTextToFileDel;
+            LogTextToScreenDel = new LogDel(log.LogText);
+            LogTextToFileDel = new LogDel(log.LogTextToFile);
+
+            Console.WriteLine("Please enter your name:");
+            var name = Console.ReadLine();
+
+            LogDel multiLogDel = LogTextToFileDel + LogTextToScreenDel;
+            LogText(multiLogDel, name);
 
         }
-        static void LogText(string text)
+        //static void LogText(string text)
+        //{
+        //    Console.WriteLine($"{DateTime.Now}: {text}");
+        //}
+        //static void LogTextToFile(string text)
+        //{
+        //    using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+        //    {
+        //        sw.WriteLine($"{DateTime.Now}: {text}");
+        //    }
+        //}
+        static void LogText(LogDel logDel, string text)
         {
-            Console.WriteLine($"{DateTime.Now}: {text}");
+            logDel(text);
+        }
+        public class Log
+        {
+            public void LogText(string text)
+            {
+                Console.WriteLine($"{DateTime.Now}: {text}");
+            }
+            public void LogTextToFile(string text)
+            {
+                using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+                {
+                    sw.WriteLine($"{DateTime.Now}: {text}");
+                }
+            }
         }
     }
 }
